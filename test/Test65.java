ac5621662c2e087eb2920b69eb8a46f736ebd490
Emma Söderberg <emma.soderberg@cs.lth.se>
2010-11-18 16:00:27 +0000

    New test

---
+++ test/Test65.java
@@ -0,0 +1,9 @@
+package test;
+import test.ast.*;
+public class Test65 {
+  public static void main(String[] args) {
+    // Refinement of syn equations that uses refined() to invoke old implementation
+    new X().m();
+    System.out.println(new X().n());
+  }
+}
