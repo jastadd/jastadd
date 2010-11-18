fc9ba0608765a65e84959f0adc69d205d9d1b6f2
Emma Söderberg <emma.soderberg@cs.lth.se>
2010-11-18 15:36:53 +0000

    New test

---
+++ test/Test58.java
@@ -0,0 +1,27 @@
+package test;
+import test.ast.*;
+public class Test58 {
+	
+	public static void main(String[] args) {
+		System.out.println("58, Circular: no stack of strongly connected components");
+		A root = new A();
+    System.out.println("Order: c1 before c2");
+		System.out.println("c1_a() = " + root.c1_a());
+		System.out.println("c2_a() = " + root.c2_a());
+		System.out.println("c1_acount " + root.c1_acount);
+		System.out.println("c1_bcount " + root.c1_bcount);
+		System.out.println("c3count " + root.c3count);
+		System.out.println("c2_acount " + root.c2_acount);
+		System.out.println("c2_bcount " + root.c2_bcount);
+    
+		root = new A();
+    System.out.println("Order: c2 before c1");
+		System.out.println("c2_a() = " + root.c2_a());
+		System.out.println("c1_a() = " + root.c1_a());
+		System.out.println("c1_acount " + root.c1_acount);
+		System.out.println("c1_bcount " + root.c1_bcount);
+		System.out.println("c3count " + root.c3count);
+		System.out.println("c2_acount " + root.c2_acount);
+		System.out.println("c2_bcount " + root.c2_bcount);
+	}
+}
