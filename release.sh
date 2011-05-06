f6b6e378e113a85eb3cfe4d6f9f48cb547811b60
Görel Hedin <gorel@cs.lth.se>
2011-05-06 20:36:58 +0000

    Small updates to release process.

--- release.sh
+++ release.sh
@@ -2,6 +2,15 @@
 VERSION=R`date +%Y%m%d`
 echo Do the following commands to create and upload jastadd2 version ${VERSION}
 
+# Patch the html files to use the correct version number
+echo "sed -e '/JastAdd2 Release R/ s/Release R......../Release '${VERSION}'/' doc/index.html >doc/index.html.new"
+echo rm doc/index.html
+echo mv doc/index.html.new doc/index.html
+
+echo "sed -e '/Reference manual for JastAdd2 R/ s/JastAdd2 R......../JastAdd2 '${VERSION}'/' doc/reference-manual.html >doc/reference-manual.html.new"
+echo rm doc/reference-manual.html
+echo mv doc/reference-manual.html.new doc/reference-manual.html
+
 # Create a zip file jastadd2-src.zip for the source distribution
 echo make source-zip
 #make source-zip
@@ -15,7 +24,7 @@ echo ssh login.cs.lth.se \"cd /cs/jastadd/releases/jastadd2 \&\& mkdir ${VERSION
 #ssh login.cs.lth.se "cd /cs/jastadd/releases/jastadd2 && mkdir ${VERSION}"
 
 # Upload the zip files and appropriate documentation to jastadd.org
-echo scp jastadd2-src.jar jastadd2-bin.zip doc/*.html doc/*.php login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}
+echo scp jastadd2.jar jastadd2-src.zip jastadd2-bin.zip doc/*.html doc/*.php login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}
 #scp jastadd2-src.jar jastadd2-bin.zip doc/*.html doc/*.php login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}
 
 # Cleaning up
