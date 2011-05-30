eb7c1493ad067532e5c9772c346a2456de161ae6
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2011-05-30 11:27:26 +0000

    Update svn ignore to ignore compiled class files.

--- jastadd/JastAdd.java
+++ jastadd/JastAdd.java
@@ -12,7 +12,7 @@ import jrag.*;
 
 public class JastAdd {
   
-    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110506";
+    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110530";
     public static final String VERSIONINFO = "\n// Generated with " + VERSION + "\n\n";
 
     protected java.util.List files;
