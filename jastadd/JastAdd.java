70693c9371070618f89f2a87f595473a6897cef0
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2011-07-27 13:14:57 +0000

    ClassBodyDecl modifiers no longer treated as comments

--- jastadd/JastAdd.java
+++ jastadd/JastAdd.java
@@ -12,7 +12,7 @@ import jrag.*;
 
 public class JastAdd {
   
-    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110601";
+    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110727";
     public static final String VERSIONINFO = "\n// Generated with " + VERSION + "\n\n";
 
     protected java.util.List files;
