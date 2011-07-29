88f7ea6de95259c17cdf6cac0799809edf71c023
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2011-07-29 17:03:54 +0000

    Comment formating fix for non-javadoc comments

--- jastadd/JastAdd.java
+++ jastadd/JastAdd.java
@@ -12,7 +12,7 @@ import jrag.*;
 
 public class JastAdd {
   
-    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110727";
+    public static final String VERSION = "JastAdd II (http://jastadd.org) version R20110729";
     public static final String VERSIONINFO = "\n// Generated with " + VERSION + "\n\n";
 
     protected java.util.List files;
