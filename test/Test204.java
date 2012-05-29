ec96667a035aca0b93ecc574a14cc7c1a8dc0312
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2012-05-29 12:57:51 +0000

    Added test for inherited equation with missing return statement.

---
+++ test/Test204.java
@@ -0,0 +1,10 @@
+package test;
+
+import test.ast.*;
+
+public class Test204 {
+    public static void main(String[] args) {
+        System.out.println("Compile should fail if the equation for an " +
+                "inherited attribute does not contain a return statement.");
+    }
+}
