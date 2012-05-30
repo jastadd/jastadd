9b51ad41aa56293be0fa76edb0f955380af7aef4
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2012-05-30 07:23:15 +0000

    It is no longer possible to comple inherited equations lacking a return statement. Fixes bug http://svn.cs.lth.se/trac/jastadd-trac/ticket/70

--- test/Test204.java
+++ test/Test204.java
@@ -4,7 +4,6 @@ import test.ast.*;
 
 public class Test204 {
     public static void main(String[] args) {
-        System.out.println("Compile should fail if the equation for an " +
-                "inherited attribute does not contain a return statement.");
+        System.out.println();
     }
 }
