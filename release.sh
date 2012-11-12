1356bd386a82e4731de646ee9ce79bcae15378e5
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2012-11-12 12:32:17 +0000

    The release script now prints comments, not just commands

--- release.sh
+++ release.sh
@@ -1,30 +1,39 @@
 #!/bin/sh
+
+# set version string
 VERSION=R`date +%Y%m%d`
-echo Do the following commands to create and upload jastadd2 version ${VERSION}
 
-# Patch the html files to use the correct version number
-echo "sed -e '/JastAdd2 Release R/ s/Release R......../Release '${VERSION}'/' doc/index.html >doc/index.html.new"
-echo rm doc/index.html
-echo mv doc/index.html.new doc/index.html
+echo "# Follow these steps to release a new jastadd2 version ${VERSION}:"
 
-echo "sed -e '/Reference manual for JastAdd2 R/ s/JastAdd2 R......../JastAdd2 '${VERSION}'/' doc/reference-manual.html >doc/reference-manual.html.new"
-echo rm doc/reference-manual.html
-echo mv doc/reference-manual.html.new doc/reference-manual.html
+echo
+echo "# 1. Patch the html files to use the correct version number"
 
-# Create a the release zip files jastadd2-src.zip and jastadd2-bin.zip
-echo ant release
+echo "sed -e 's/\\(JastAdd2 Release \\)R[0123456789]*/"'\\1'"${VERSION}/' \\"
+echo "    doc/index.html > doc/index.html.new"
+echo "mv doc/index.html.new doc/index.html"
 
-# Create a new dir at jastadd.org
-echo ssh login.cs.lth.se \"cd /cs/jastadd/releases/jastadd2 \&\& mkdir ${VERSION}\"
+echo "sed -e 's/\\(manual for JastAdd2 \\)R[0123456789]*/"'\\1'"${VERSION}/' \\"
+echo "    doc/reference-manual.html >doc/reference-manual.html.new"
+echo "mv doc/reference-manual.html.new doc/reference-manual.html"
+
+echo
+echo "# 2. Create the release zip files jastadd2-src.zip and jastadd2-bin.zip"
+echo "ant release"
+
+echo
+echo "# 3. Create a new dir at jastadd.org"
+echo "ssh login.cs.lth.se \"cd /cs/jastadd/releases/jastadd2 &&"\
+	"mkdir ${VERSION}\""
 #ssh login.cs.lth.se "cd /cs/jastadd/releases/jastadd2 && mkdir ${VERSION}"
 
-# Upload the zip files and appropriate documentation to jastadd.org
-echo scp jastadd2.jar jastadd2-src.zip jastadd2-bin.zip doc/*.html doc/*.php login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}
+echo
+echo "# 4. Upload the zip files and appropriate documentation to jastadd.org"
+echo "scp jastadd2.jar jastadd2-src.zip jastadd2-bin.zip doc/*.html doc/*.php\\"
+echo "    login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}"
 #scp jastadd2-src.jar jastadd2-bin.zip doc/*.html doc/*.php login.cs.lth.se:/cs/jastadd/releases/jastadd2/${VERSION}
 
-# Cleaning up
-echo rm -f jastadd2-bin.zip jastadd2-src.zip
-#rm -f jastadd2-bin.zip jastadd2-src.zip
+echo "# 5. Cleaning up"
+echo "rm -f jastadd2-bin.zip jastadd2-src.zip"
 
 # Check that it works
 echo ------------------------------------------------------
@@ -33,4 +42,6 @@ echo Then ant clean and commit.
 echo ------------------------------------------------------
 
 # Tag in SVN
-echo "svn copy http://svn.cs.lth.se/svn/jastadd/trunk/JastAdd2 http://svn.cs.lth.se/svn/jastadd/tags/JastAdd2/${VERSION} -m \"Release ${VERSION} of JastAdd2\" "
+echo "svn copy http://svn.cs.lth.se/svn/jastadd/trunk/JastAdd2 \\"
+echo "    http://svn.cs.lth.se/svn/jastadd/tags/JastAdd2/${VERSION} \\"
+echo "    -m \"Release ${VERSION} of JastAdd2\""
