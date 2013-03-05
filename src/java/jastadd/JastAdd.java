839dde1518bbff842c5379d28ae79ae3b804d2a8
Jesper Öqvist <jesper.oqvist@cs.lth.se>
2013-03-05 13:05:17 +0100

    Refactoring code generation

--- src/java/jastadd/JastAdd.java
+++ src/java/jastadd/JastAdd.java
@@ -622,6 +622,7 @@ public class JastAdd {
       tt.bind("SynchBegin", "");
       tt.bind("SynchEnd", "");
     }
+    tt.bind("CacheCycleEnabled", root.cacheCycle);
     tt.bind("Java5", root.java5);
     tt.bind("VisitCheckEnabled", root.visitCheckEnabled);
     tt.bind("TraceVisitCheck", root.traceVisitCheck);
